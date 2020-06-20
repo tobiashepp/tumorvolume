6af8fb7368bc0ba88960e4065b817935cfc79c10
Tobias Hepp <tobias.hepp@med.uni-tuebingen.de>
2020-06-20 17:16:01 +0000

    validation

--- tumorvolume/preprocessing/process_ctorgans.py
+++ tumorvolume/preprocessing/process_ctorgans.py
@@ -11,24 +11,26 @@ from midastools.misc.orientation import reorient_nii
 
 work_dir = Path("/mnt/qdata/raheppt1/data/tumorvolume/")
 zip_path = work_dir/"raw/TUE0000CTORG_pp.zip"
-h5_path = work_dir/"interim/ctorgans.h5"
+h5_path = work_dir/"interim/ctorgans/ctorgans.h5"
 ct_range = [-1000.0, 1000.0]
 target_orientation = ("L","A","S")
 
 # extract nifti data from zip file, reorient, normalize 
 # and write to hdf5
 # nifti data is assumed to be resampled to (2,2,3)mm and must be LAS compatibel
+# CAVE: There is a orientation bug in some of the zipped nifti masks!!!
+# LPI should be LPS!!! Therefore, the affine matrix is manually corrected.
 with h5py.File(h5_path, "w") as hf:
     with ZipFile(zip_path, "r") as zf:
         # parse filelist
         file_list = zf.namelist()
-        label_files = [f for f in file_list if re.match(".*label.*.nii.gz", f)]
-        img_files = [f for f in file_list if re.match(".*ct.*.nii.gz", f)]
+        label_files = [f for f in file_list if re.match(".*labels_3mm.*.nii.gz", f)]
+        img_files = [f for f in file_list if re.match(".*ct_3mm.*.nii.gz", f)]
 
         for flabel, fimg in zip(label_files, img_files):
             # get keys
             print(flabel, fimg)
-            key_label = re.match(".*/(\d{3}).*nii.gz", flabel)[1]
+            key_label = re.match(".*/(\d{3}).*nii.gz", fimg)[1]
             key_img   = re.match(".*/(\d{3}).*nii.gz", flabel)[1]
             assert key_img == key_label
 
@@ -47,6 +49,11 @@ with h5py.File(h5_path, "w") as hf:
             print(f"img  : {img.shape}, {img.header.get_zooms()}, {nib.aff2axcodes(img.affine)}")
             print(f"label: {label.shape}, {label.header.get_zooms()}, {nib.aff2axcodes(label.affine)}")
 
+            if nib.aff2axcodes(img.affine)[2] == "I":
+                print("fixing orientation bug {nib.aff2axcodes(img.affine)}!") 
+                # Bug fixing: LPI -> LPS for corrupted images
+                img.affine[:, 2] = -img.affine[:, 2]
+
             # reorient niftis 
             img = reorient_nii(img, target_orientation)
             label = reorient_nii(label, target_orientation)
@@ -64,10 +71,12 @@ with h5py.File(h5_path, "w") as hf:
             gr = hf.require_group("images")
             ds = gr.require_dataset(key_img, img_array.shape, img_array.dtype)
             ds[:] = img_array
+            ds.attrs["affine"] = img.affine.tolist()
 
             gr = hf.require_group("labels")
             ds = gr.require_dataset(key_label, label_array.shape, label_array.dtype)
             ds[:] = label_array
+            ds.attrs["affine"] = label.affine.tolist()
 
 
 # generate key files for cross validation
@@ -81,3 +90,18 @@ for k, (train_index, test_index) in enumerate(kf.split(subject_keys)):
     test_keys = [(subject_keys[i] + "\n") for i in test_index]
     with Path(h5_path.parent/(h5_path.stem+f"_test{k}.dat")).open("w") as keyfile:
         keyfile.writelines(test_keys)
+
+import matplotlib.pyplot as plt
+validation_dir = (h5_path.parent/f"validation")
+validation_dir.mkdir(exist_ok=True)
+with h5py.File(h5_path, "r") as hf:
+    subject_keys = list(hf["images"])
+    for k in subject_keys:
+        print(k)
+        img = hf[f"images/{k}"][:].astype(np.float32)
+        mask = hf[f"labels/{k}"][:].astype(np.float32)
+        fig, ax = plt.subplots()
+        ax.imshow(img[0].max(axis=1), cmap="bone")
+        ax.imshow(mask[0].max(axis=1), cmap="coolwarm", alpha=0.5)
+        plt.savefig(str(validation_dir/f"validation{k}.png"))
+        plt.close(fig)
