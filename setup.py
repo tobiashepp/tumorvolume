442bd66740387ae1decf18fc3946d922a94d7af8
Tobias Hepp <tobias.hepp@med.uni-tuebingen.de>
2020-07-05 16:27:17 +0000

    new setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='tumorvolume',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Medical image segmentation and landmark detection.',
+    author='Bjarne Kerber, Dr. Tobias Hepp',
+    license='MIT',
+)
